4374a8de81f2358d431325d7ae1cd575a52a3a1a
ImBuny <79527158+ImBuny@users.noreply.github.com>
2024-10-20 15:22:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Exchange now at 2% FEES!"];
+const statusMessages = ["30% Extra on first order"];
 const statusTypes = ['dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
